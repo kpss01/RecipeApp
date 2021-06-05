ab5e79e7ca06727fbdc2d8bbe5f4029c396ca164
kpss <saraohealthcare@gmail.com>
2021-06-06 02:29:49 +0530

    Add RecipeFeaturedView and EnvironmentObject

---
+++ ListView/View/RecipeFeaturedView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeFeaturedView.swift
+//  ListView
+//
+//  Created by karanpreet singh sarao on 06/06/21.
+//
+
+import SwiftUI
+
+struct RecipeFeaturedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeFeaturedView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeFeaturedView()
+    }
+}
